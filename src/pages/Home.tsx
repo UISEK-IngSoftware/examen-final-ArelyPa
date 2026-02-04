4bc43f6d3d67c5c7123cdd55e7474744f86e662f
laboratorio1.fs <arely.pazmino@uisek.edu.ec>
2026-02-04 09:37:39 -0500

    Examen Final

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,56 +1,128 @@
-import MessageListItem from '../components/MessageListItem';
-import { useState } from 'react';
-import { Message, getMessages } from '../data/messages';
-import {
-  IonContent,
-  IonHeader,
-  IonList,
-  IonPage,
-  IonRefresher,
-  IonRefresherContent,
-  IonTitle,
-  IonToolbar,
-  useIonViewWillEnter
+import { IonAvatar, IonCard, IonCardContent, IonContent, IonHeader, IonItem, IonLabel, IonList, IonLoading, IonPage, IonRefresher, IonRefresherContent, IonText, IonTitle,
+  IonToolbar, useIonViewWillEnter
 } from '@ionic/react';
+import { useState } from 'react';
+import axios from 'axios';
 import './Home.css';
 
+// Interface del personaje
+interface Character {
+  id: number;
+  name: string;
+  gender: string;
+  status: string;
+  species: string;
+  createdAt: string;
+  image: string;
+}
+
 const Home: React.FC = () => {
+  // Estados (alamcenar lista de personajes, control de carga de datos y errores)
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
-  const [messages, setMessages] = useState<Message[]>([]);
+  // Llamada de API
+  const loadCharacters = async () => {
+    setLoading(true);
+    setError(null);
 
+    try {
+      const response = await axios.get(
+        'https://futuramaapi.com/api/characters',
+        {
+          params: {
+            orderBy: 'id',
+            orderByDirection: 'asc',
+            page: 1,
+            size: 50
+          }
+        }
+      );
+
+      setCharacters(response.data.items);
+    } catch (error) {
+      console.error(' Erro: No se pudieron cargar los personajes:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Se cargan los datos al iniciar la página
   useIonViewWillEnter(() => {
-    const msgs = getMessages();
-    setMessages(msgs);
+    loadCharacters();
   });
 
-  const refresh = (e: CustomEvent) => {
-    setTimeout(() => {
-      e.detail.complete();
-    }, 3000);
+  // Refresh (vuelve a consultar API)
+  const refresh = async (e: CustomEvent) => {
+    await loadCharacters();
+    e.detail.complete();
   };
 
   return (
     <IonPage id="home-page">
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Inbox</IonTitle>
+          <IonTitle>Personajes de Futurama</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
         <IonRefresher slot="fixed" onIonRefresh={refresh}>
-          <IonRefresherContent></IonRefresherContent>
+          <IonRefresherContent />
         </IonRefresher>
 
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">
-              Inbox
-            </IonTitle>
+            <IonTitle size="large">Futurama</IonTitle>
           </IonToolbar>
         </IonHeader>
 
+        {/* Indicador de carga */}
+        <IonLoading isOpen={loading} message="Cargando personajes..." />
+
+        {/* Mensaje de Error */}
+        {error && (
+          <IonText color="danger" className="ion-padding">
+            <p>{error}</p>
+          </IonText>
+        )}
+
+        {/* Estado vacío */}
+        {!loading && !error && characters.length === 0 && (
+          <IonText className="ion-padding">
+            <p>No hay personajes disponibles.</p>
+          </IonText>
+        )}
+
+        {/* Lista de los personajes */}
         <IonList>
-          {messages.map(m => <MessageListItem key={m.id} message={m} />)}
+          {characters.map(character => (
+            <IonCard key={character.id}>
+              <IonItem lines="none">
+                <IonAvatar slot="start" className="character-avatar"> 
+                  <img src={character.image} alt={character.name} />
+                </IonAvatar>
+
+               <IonLabel>
+                  <h2 className="character-name">{character.name}</h2>
+                  <p className="character-meta">
+                    Género: {character.gender}              
+                    </p>
+                    <p>
+                    Especie: {character.species}
+                    </p>
+                    <p>
+                    Estado: {character.status}
+                    </p>
+                </IonLabel>
+              </IonItem>
+
+              <IonCardContent>
+                
+              </IonCardContent>
+            </IonCard>
+          ))}
         </IonList>
       </IonContent>
     </IonPage>
